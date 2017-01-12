3617c3457cb1619ca0b2cad143d605f15aa5386b
jasonwoo <wuyingminhui@gmail.com>
2017-01-12 13:12:07 +0800

    add manual unlock function

--- Ubike/manual/manual.js
+++ Ubike/manual/manual.js
@@ -2,41 +2,75 @@ var app = getApp()
 Page({
   data: {
     index: 0,
-    enable: false,
+    enable: true,
     postdata: {0: '', 1: '', 2: '', 3: '', 4: '', 5: '', 6: '', 7: '', 8: '', 9: '', 10: ''}
   },
   onLoad: function(option) {
     
   },
   inpp: function(e) {
-      console.log(e)
+      // console.log(e)
       var that = this
       if(e.detail.cursor == 1){
         if(that.data.index < 11){
           var tmp = that.data.postdata
           tmp[e.target.id] = e.detail.value
-          if (this.getNum().length === 11) {
-            that.setData({
-                enable : true
-            })
-          }
+          that.setData({
+                postdata : tmp
+          })
+          this.checkDisable()
           that.setData({
                 index : that.data.index + 1
           })
+          // console.log(tmp)
         } else {
           var tmp = that.data.postdata
           tmp[e.target.id] = e.detail.value
-          if (this.getNum().length === 11) {
-            that.setData({
-                enable : true
-            })
-          }
+          that.setData({
+                postdata : tmp
+          })
+          this.checkDisable()
+          // console.log(tmp)
+        }
+      } else {
+        if (e.target.id < 11) {
+          var tmp = that.data.postdata
+          tmp[e.target.id] = e.detail.value
+          that.setData({
+                postdata : tmp
+          })
+          this.checkDisable()
           // console.log(tmp)
         }
       }
   },
+  checkDisable: function () {
+    var that = this
+    if (this.getNum().length === 11) {
+      that.setData({
+          enable : false
+      })
+    } else {
+      that.setData({
+          enable : true
+      })
+    }
+  },
   manully: function() {
-    this.getNum()
+    var bikenum = this.getNum()
+    if (bikenum.length !== 11) {
+        wx.showModal({
+            title: '警告',
+            content: '请输入正确的车辆编号',
+            showCancel: false,
+            success: function(res) {
+            }
+        })
+    } else {
+      wx.redirectTo({
+          url: '../unlock/unlock?bn=' + bikenum
+      })
+    }
   },
   getNum: function() {
     var that = this
